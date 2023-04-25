4187e6b0f8746fce338c90aa54f0387ae621cc0f
MD-SALMAN-HOSSAIN <salman00hossain@gmail.com>
2023-04-26 01:34:04 +0600

    1st push

---
+++ E-Ticketing/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="E_Ticketing.WebApiApplication" Language="C#" %>
---
+++ E-Ticketing/Global.asax.cs
@@ -0,0 +1,12 @@
+using System.Web.Http;
+
+namespace E_Ticketing
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
