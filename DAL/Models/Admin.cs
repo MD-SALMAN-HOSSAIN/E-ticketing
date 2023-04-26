468fe577428c66c156ca775025ec4d3425aafbe6
DasPriYam714 <priyamdas.aiub@gmail.com>
2023-04-26 12:29:31 +0600

    admin_commit

--- DAL/Models/Admin.cs
+++ DAL/Models/Admin.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,15 @@ namespace DAL.Models
 {
     class Admin
     {
-        ////
+        [Key]
+       public string Uname { get; set; }
+        [Required]
+        [StringLength(30)]
+       public string Password { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Name { get; set; }
+
+
     }
 }
