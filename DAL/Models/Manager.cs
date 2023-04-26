0f0f759991134b2823c02a9f73d1469fd90fbdfd
MD-SALMAN-HOSSAIN <salman00hossain@gmail.com>
2023-04-26 13:14:07 +0600

    commit manager.....

--- DAL/Models/Manager.cs
+++ DAL/Models/Manager.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,21 @@ namespace DAL.Models
 {
     internal class Manager
     {
+        
+
+        public string uId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string uName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string movie { get; set; }
+
+        [Required]
+        public DateTime movie_time { get; set; }
+
+
     }
 }
