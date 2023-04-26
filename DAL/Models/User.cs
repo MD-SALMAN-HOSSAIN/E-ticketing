32e7021be2105dccfba56b4f3263a8995a216fd8
DasPriYam714 <priyamdas.aiub@gmail.com>
2023-04-26 13:06:25 +0600

    user_commit

---
+++ DAL/Models/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class User
+    {
+        public int uID { get; set; }
+        [Required]
+        [StringLength(30)]
+
+        public string uName { get; set; }
+        [Required]
+        [StringLength(20)]
+
+        public string Password { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string Email { get; set; }
+        [Required]
+        public int PhoneNum { get; set; }
+        [Required]
+        public DateTime DOB { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        
+
+    }
+}
