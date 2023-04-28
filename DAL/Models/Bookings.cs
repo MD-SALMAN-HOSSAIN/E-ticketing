30a7802d69319a71a1b614c0d2d77c708a4df8e1
DasPriYam714 <priyamdas.aiub@gmail.com>
2023-04-28 10:43:59 +0600

    bookings

--- DAL/Models/Bookings.cs
+++ DAL/Models/Bookings.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DAL.Models
 {
-    class Bookings
+    public class Bookings
     {
+        [Key]
+        public int booking_ID { get; set; }
+        [ForeignKey("user")]
+        public int user_ID { get; set; }
+        [ForeignKey("ticket")]
+        public int ticket_ID { get; set; }
+        [Required]
+        public DateTime booking_Date { get; set; }
+        [Required]
+        public int payment { get; set; }
+
+        public virtual User user { get; set; }
+        public virtual ticket ticket { get; set; }
+        
     }
 }
